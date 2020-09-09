6a10264edc3eb2ae634d6eed3cfa3fb67d0b7a86
Kolyan525 <Kolyakalina2001@gmail.com>
2020-09-09 23:35:30 +0300

    Lection 09.07.2020
    Terrain, hero, move <- -> script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,14 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject player;
+    
     // Update is called once per frame
     void Update()
     {
