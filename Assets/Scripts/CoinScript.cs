4755632167ef335fb86118aa9dd6387d8b17de24
Kolyan525 <Kolyakalina2001@gmail.com>
2020-10-15 15:58:43 +0300

    Score
    Added simple score system

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    int score = 0;
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered");
+        Destroy (gameObject);
+        score += 1;
+        Debug.Log($"Score: {score}");
+    }
+}
